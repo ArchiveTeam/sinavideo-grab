d5193534257a5d46c8d5442ed01c06822cb631e2
Robin Rolf <imer@imer.cc>
2026-08-26 14:18:51 +0000

    Version 20260826.01. Use current firefox UA

--- pipeline.py
+++ pipeline.py
@@ -71,7 +71,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20260825.01'
+VERSION = '20260826.01'
 TRACKER_ID = 'sinavideo'
 TRACKER_HOST = 'legacy-api.arpa.li'
 MULTI_ITEM_SIZE = 1 # KEEP ONE
@@ -357,7 +357,8 @@ class WgetArgs(object):
             '--warc-header', 'x-wget-at-project-name: ' + TRACKER_ID,
             '--warc-dedup-url-agnostic',
             '--impersonate', 'firefox148-h1',
-            '--header', 'Accept-Encoding: identity'
+            '--header', 'Accept-Encoding: identity',
+            '--user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:154.0) Gecko/20100101 Firefox/154.0'
         ]
 
         if '--concurrent' in sys.argv:
